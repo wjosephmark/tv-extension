aa87fbb332d4b617597f191310a81a69197fef0b
Joseph Mark <wjosephmark@gmail.com>
2025-01-25 17:44:51 -0700

    fixing gitignore

---
+++ src/react/components/App.jsx
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+
+const App = () => {
+	return (
+		<main>
+			<h1>Hello Trader!</h1>
+      <button>Play Audio</button>
+		</main>
+	)
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+import path from 'path';
+
+export default {
+  mode: 'production',
+  entry: {
+		contentScript: './src/content/index.js',
+		background: './src/background/index.js',
+		react: './src/react/index.jsx'
+	},
+	output: {
+		path: path.resolve('dist'),
+		filename: '[name].js',
+		clean: true
+	},
+	resolve: {
+		extensions: ['.js', '.jsx'], // Add .jsx to recognized extensions
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new CopyPlugin({
+			patterns: [{
+				from: path.resolve('manifest.json'),
+				to: path.resolve('dist')
+			}]
+		})
+	],
+	module: {
+		rules: [
+			{
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader'], 
+      },
+			{
+				test: /.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'@babel/preset-env',
+							['@babel/preset-react', {'runtime': 'automatic'}]
+						]
+					}
+				}
+			}
+		]
+	}
+}
